ba8996522dcae262ec90c552528bad984adca499
Remco Troost <remcotroost@outlook.com>
2017-03-30 00:31:37 +0200

    Secure login vs bruteforce attacks via cookie (untested)

--- ITA/login.php
+++ ITA/login.php
@@ -32,8 +32,25 @@ include 'core/init.php';
       $login = login($username, $password);
       if($login === false)
       {
-           
-            $errors[] = 'That username and password combination is incorrect!';
+			if(isset($_COOKIE['attempts'])) 
+			{
+				if($_COOKIE['attempts'] < 3) 
+				{
+			    	$attempts = $_COOKIE['attempts'] + 1;
+			    	$remaining = 3 - $attempts;
+			    	setcookie('attempts', $attempts, time()+60*10); //cookie for 10 min + store attempts
+			    	$errors[] = 'That username and password combination is incorrect! Tries remaining: ' + $remaining + '.';
+				} 
+				else 
+				{
+			    	$errors[] = 'You have tried to login 3 times! You are now banned for 10 minutes';
+				}
+			} 
+			else 
+			{
+				setcookie('attempts', 1, time()+60*10); //cookie for 10 min + attempts to 1
+				$errors[] = 'That username and password combination is incorrect!';
+			}
       }
       else 
       {
